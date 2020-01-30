07c03366637882f92cbbe5c9fc43a54bd0ffe7fe
samuel-shard <59847990+samuel-shard@users.noreply.github.com>
2020-01-30 12:47:11 -0500

    Add files via upload

--- rev_file.cpp
+++ rev_file.cpp
@@ -1,3 +1,6 @@
+//Week 4 Activity 2
+//January 30, 2020
+//Author: Samuel Shard, Student Number: 100745640
 #include <iostream>
 #include <fstream>
 #include <cstring>
@@ -46,32 +49,39 @@ void freadnames(ifstream &f,char *list [])
 
 	int i = 0;
 
-	// write a while loop to reads string form the file and put it in x till the end file
-		// inside the loop allocate the dynamic array for list[i]
-		// copy string in x to list[i] array
+	// write a while loop to read one string from the input file 
+	// and put it in x till the end of the file
+		// inside the loop, allocate the dynamic array for list[i]
+		// copy string stored in x to list[i] array using strcpy
 		// increment i 
-
-	list[i] = nullptr;  // We put the null to the last pointer to mark that the last element in list
+	while(f >> x){
+		list[i] = new char[strlen(x)+1]; //first step
+		strcpy(list[i], x);
+		i++;
+	}
+	
+	// IMPORTANT: we put the null to the last pointer in list 
+	// to mark the last element in list
+	list[i] = nullptr;  
 }
 void fwritenames_reverse(ofstream &f,char *list [])
 {
 	int i;
-	for(i = 0; list[i] != nullptr ; ++i)
-		;
+	for(i = 0; list[i] != nullptr ; i++)
+	;//list i points to nullptr (one past the end of the array)
 
-	for(int j = i-1; j >= 0 ; --j)
-	{
-		// your code is here
+	//starting at the end of the array, go in reverse
+	for(int j = i-1; j >= 0 ; j--)
+	{	
+		//write names starting from bottom to top of list to outfile
+		f << list[j] << "\n";
+		cout << list[j] << " ";
 	}
 }
 void freenames(char *list [])
 {
-	for (int i = 0; list[i] != nullptr; ++i)
+	for (int i = 0; list[i] != nullptr; i++)
 	{
-		delete [] list[i];;
+		delete [] list[i];
 	}
 }
-
-
-
-
